248aa1d12dc27b1f1d49ce07e1450540fd513bf3
pernebay-arailym <pernebayarailym@gmail.com>
2024-12-04 15:35:44 +0100

    activated debugger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask
-from flask_restful import Api, Resourse
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+
+if __name__ == "__main__":
+    app.run(debug=True) # in debug moods we see what is wrong

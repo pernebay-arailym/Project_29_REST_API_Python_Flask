8d0d27fa60d788cf56df060254ba0e8692ebbd2e
pernebay-arailym <pernebayarailym@gmail.com>
2024-12-04 15:42:44 +0100

    created class, registeres as resource and added name of the class

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+class HelloWorld(Resource): #created class with resource which will have a few different methods
+    def get(self):
+        return {"Hello World"} # newt step we register this as a resource:
+    
+api.add_resource(HelloWorld, "/helloworld") #added  name of the class
+
 if __name__ == "__main__":
     app.run(debug=True) # in debug moods we see what is wrong

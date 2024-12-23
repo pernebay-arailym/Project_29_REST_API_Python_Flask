38c17e75ee570d4069678a6b8b83ce6e902fd8f3
pernebay-arailym <pernebayarailym@gmail.com>
2024-12-23 17:33:06 +0100

    get & put with new test parametres

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
-calledUrl = BASE + "helloworld/bill", {"likes": 10}
-response = requests.put(calledUrl, headers={"Content-Type": "application/json"}) #wanna send get request to the url
+#calledUrl = BASE + "video/1", {"likes": 10}
+response = requests.put(BASE + "video/1", json={"likes": 10, "name": "Ary", "views": 100000}) # (calledUrl, headers={"Content-Type": "application/json"}) #wanna send get request to the url
+#try:
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
+print(response.json())
 
-try:
 
-
-    print('whaaat ', response.json())
-
-
-
-except requests.exceptions.JSONDecodeError:
-    print("Response was not JSON, got:", response)
+#except requests.exceptions.JSONDecodeError:
+    #print("Response was not JSON, got:", response)

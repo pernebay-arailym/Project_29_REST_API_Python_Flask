3c73c501ddc6643b3ece130c93d093a46ebb2f60
pernebay-arailym <pernebayarailym@gmail.com>
2024-12-23 12:29:21 +0100

    added name and age arametres to the get request

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {"ary": {"age": 24, "gender": "female"},
+         "bill": {"age": 27, "gender": "male"}}
+
 class HelloWorld(Resource): #created class with resource which will have a few different methods
-    def get(self):
-        return {"data": "Hello World"} # newt step we register this as a resource, JSON Format used, LOOK IF IT IS JSON SERIARISABLE 
+    def get(self, name):
+        return names[name] # newt step we register this as a resource, JSON Format used, LOOK IF IT IS JSON SERIARISABLE 
     
-    def post(self):
-        print("POST request received!")  # Log the request
-        return {"data": "Posted"}
+    #def post(self):
+        #print("POST request received!")  # Log the request
+        #return {"data": "Posted"}
     
-api.add_resource(HelloWorld, "/helloworld") #added  name of the class
+api.add_resource(HelloWorld, "/helloworld/<string:name>") #added  name of the class
 
 if __name__ == "__main__":
     app.run(debug=True) # in debug moods we see what is wrong

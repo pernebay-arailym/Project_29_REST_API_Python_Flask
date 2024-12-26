f8c71c886f03c615207364a49a13b7c641278ee0
pernebay-arailym <pernebayarailym@gmail.com>
2024-12-26 14:18:07 +0100

    created update/patch class for arguments

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ video_put_args.add_argument("name", type=str, help="Name of the video is require
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required") #user send any of the arguments to update
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes of the video")
+
+
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,  #define what i want in this field
@@ -48,16 +54,27 @@ class Video(Resource):
     @marshal_with(resource_fields)
     def get(self, video_id):
         result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that id")
         return result
-    
+
+    @marshal_with(resource_fields)
     def put(self, video_id):
        # print(request.form['likes'])
         args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video is taken...")
+
         video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
         db.session.add(video)
         db.session.commit()
         return video, 201
-    
+
+
+    def patch(self, video_id):
+        args = video_put_args.parse_args()
+
     def delete(self, video_id):
         abort_if_video_id_doesnot_exist(video_id)
         del videos[video_id]
